1c0ef075c55c96c2ffdab44d67c7e77ab2d32c6c
Anamol Soman <anamolsoman2@gmail.com>
2025-02-03 11:39:42 +0530

    Configured tailwind 4.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,7 @@
-import { ChangeEvent, MouseEvent, useEffect, useState } from "react";
 import "./App.css";
-import { RootState } from "@reduxjs/toolkit/query";
-import { useDispatch, useSelector } from "react-redux";
-import { User } from "./interfaces";
-import { addUser, setUsers } from "./features/userSlice";
 
 function App() {
-  const dispatch = useDispatch();
-  const [user, setUser] = useState<User | null>();
-  const users = useSelector((state: RootState) => state.users.users);
-  console.log(users, "users");
-  const setUserData = (e: MouseEvent<HTMLButtonElement>) => {
-    dispatch(addUser());
-  };
-
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((res) => res.json())
-      .then((data) => {
-        dispatch(setUsers(data));
-      });
-  }, []);
-
-  return (
-    <>
-      {users &&
-        users.map((user: User) => {
-          return (
-            <>
-              <div>Name : {user?.name}</div>
-              <div>Email : {user?.email}</div>
-              <div>City : {user?.city}</div>
-              <div>Id : {user?.id}</div>
-            </>
-          );
-        })}
-
-      <button onClick={setUserData}>Set User</button>
-      <p>Click on the Vite and React logos to learn more</p>
-    </>
-  );
+  return <div className="container min-h-screen min-w-screen"></div>;
 }
 
 export default App;
---
+++ src/components/Users.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { useEffect } from "react";
+
+import { RootState } from "@reduxjs/toolkit/query";
+import { useDispatch, useSelector } from "react-redux";
+import { User } from "./interfaces";
+import { addUser, setUsers } from "./features/userSlice";
+function Users() {
+  const dispatch = useDispatch();
+  // const [user, setUser] = useState<User | null>();
+  const users = useSelector((state: RootState) => state.users.users);
+  console.log(users, "users");
+  const setUserData = () => {
+    dispatch(addUser());
+  };
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((data) => {
+        dispatch(setUsers(data));
+      });
+  }, []);
+
+  return (
+    <div>
+      {" "}
+      {users &&
+        users.map((user: User) => {
+          return (
+            <>
+              <div className="text-red">Name : {user?.name}</div>
+              <div>Email : {user?.email}</div>
+              <div>City : {user?.city}</div>
+              <div>Id : {user?.id}</div>
+            </>
+          );
+        })}
+      <button onClick={setUserData}>Set User</button>
+    </div>
+  );
+}
+
+export default Users;
